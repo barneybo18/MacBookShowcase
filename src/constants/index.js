7107fea92a12c4ebfcf0be4c5200fe41d3cbd0b1
Barnabas Oboh <bannyzinnovate@gmail.com>
2025-11-15 22:17:54 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const navLinks = [
+    { label: 'store' },
+    { label: 'mac' },
+    { label: 'ipad' },
+    { label: 'iphone' },
+    { label: 'watch' },
+    { label: 'Vision' },
+    { label: 'music' },
+]
