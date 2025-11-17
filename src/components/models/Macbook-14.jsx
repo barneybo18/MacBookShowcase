9f2b6f00b5ea5f95d087581d84817084a1afaa01
Barnabas Oboh <bannyzinnovate@gmail.com>
2025-11-17 18:50:47 +0100

    Fixed the color changing problems on th 14 inch macbook

--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -20,21 +20,17 @@ export default function MacbookModel14(props) {
 
   const texture = useTexture('/screen.png');
   texture.colorSpace = SRGBColorSpace;
-  texture.needsUpdate = true;
+    texture.needsUpdate = true;
 
   useEffect(() => {
     scene.traverse((child) => {
-      if (child.isMesh) {
-        if(!noChangeParts.includes(child.name)){
-          if (!child.material.userData.cloned) {
-            child.material = child.material.clone();
-            child.material.userData.cloned = true;
+          if (child.isMesh) {
+            if(!noChangeParts.includes(child.name)){
+              child.material.color = new Color(color);
+            }
           }
-          child.material.color = new Color(color);
-        }
-      }
-    })
-  }, [color, scene])
+        })
+      }, [color, scene]);
 
   return (
     <group {...props} dispose={null}>
