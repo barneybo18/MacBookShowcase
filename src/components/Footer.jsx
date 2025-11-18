4154998552c8a884890848f99182ecc33a5e1fdc
Barnabas Oboh <bannyzinnovate@gmail.com>
2025-11-18 15:38:56 +0100

    Implemented the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='info'>
+        <p>More Ways to shop: Find an Apple Store or other retailer near you. Or call +123456789.</p>
+        <img src="/apple.svg" alt="apple logo" />
+      </div>
+
+        <hr />
+
+        <div className='links'>
+          <p>Copyright © 2025 Apple Inc. All rights reserved.</p>
+
+          <ul>
+            {
+              footerLinks.map(({label, link}) => (
+                <li key={label}>
+                  <a href={link}>{label}</a>
+                </li>
+              ))
+            }
+          </ul>
+      </div>
+    </footer>
   )
 }
 
